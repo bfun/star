0a07ac9ef660529d412329279075798003409df9
nil <zilongqi@hotmail.com>
2024-05-10 19:26:02 +0800

    v0.0.2

--- handler.go
+++ handler.go
@@ -6,9 +6,39 @@ import (
 )
 
 func dtaHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, ParseAllDtaParmXml())
+	dtaName := c.Query("dta")
+	dtas := ParseAllDtaParmXml()
+	var v any
+	if dtaName != "" {
+		dta, ok := dtas[dtaName]
+		if ok {
+			v = dta
+		} else {
+			v = gin.H{dtaName: "not found"}
+		}
+	} else {
+		v = dtas
+	}
+	c.JSON(http.StatusOK, v)
 }
 
 func svcHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, ParseAllServiceXml())
+	dtaName := c.Query("dta")
+	svcName := c.Query("svc")
+	svcs := ParseAllServiceXml()
+	var v any
+	if dtaName != "" {
+		dta, ok := svcs[dtaName]
+		if ok {
+			v, ok = dta[svcName]
+			if !ok {
+				v = gin.H{svcName: "not found"}
+			}
+		} else {
+			v = gin.H{dtaName: "not found"}
+		}
+	} else {
+		v = svcs
+	}
+	c.JSON(http.StatusOK, v)
 }
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func Main() {
 	r := gin.Default()
-	r.GET("/dta", dtaHandler)
-	r.GET("/svc", svcHandler)
+	r.GET("/dta/:dta", dtaHandler)
+	r.GET("/svc/:dta/:svc", svcHandler)
 	r.Run(":8080")
 }
