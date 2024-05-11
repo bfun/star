047385398f1a895baeafd503c90f15937c0ed6b7
nil <zilongqi@hotmail.com>
2024-05-12 00:01:09 +0800

    v0.0.21

--- route.go
+++ route.go
@@ -11,16 +11,16 @@ type RouteTab struct {
 }
 
 type Rule struct {
-	RuleID    string       `xml:"RuleID,attr"`
-	RuleType  string       `xml:"RuleType,attr"`
-	SvcExpr   xml.CharData `xml:"SvcExpr"`
-	RouteExpr xml.CharData `xml:"RouteExpr"`
-	Entrances []Entrance   `xml:"EntranceTab>Entrance"`
+	RuleID    string     `xml:"RuleID,attr"`
+	RuleType  string     `xml:"RuleType,attr"`
+	SvcExpr   string     `xml:"SvcExpr"`
+	RouteExpr string     `xml:"RouteExpr"`
+	Entrances []Entrance `xml:"EntranceTab>Entrance"`
 }
 type Entrance struct {
-	Destination string       `xml:"Destination,attr"`
-	DstType     string       `xml:"DstType,attr"`
-	Expr        xml.CharData `xml:"Expr"`
+	Destination string `xml:"Destination,attr"`
+	DstType     string `xml:"DstType,attr"`
+	Expr        string `xml:"Expr"`
 }
 
 type CDATA struct {
@@ -38,7 +38,7 @@ func parseOneRouteXml(fileName string) map[string]Entrance {
 	m := make(map[string]Entrance)
 	for _, r := range v.Rules {
 		for _, e := range r.Entrances {
-			m[string(e.Expr)] = e
+			m[e.Expr] = e
 		}
 	}
 	return m
